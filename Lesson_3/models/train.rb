b767af788525f143ed27b60951257528bf3670f3
nickkaylarson <nicholaskaylarson@gmail.com>
2021-07-27 15:16:59 -0500

    add initialize, stop, change wagon amount methods

--- Lesson_3/models/train.rb
+++ Lesson_3/models/train.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 
 class Train
-  attr_reader :name, :type
-  attr_accessor :wagons_amount, :speed, :route
+  attr_reader :number, :type
+  # cargo, passenger
+  attr_accessor :speed, :route
 
-  def initialize; end
+  def initialize(name, type, wagons_amount)
+    @name = name
+    @type = type
+    @wagons_amount = wagons_amount
+    @speed = 0
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def change_wagon_amount(amount)
+    if @speed.zero?
+      @wagons_amount += amount
+    else
+      p 'Please, stop the train first!'
+    end
+  end
 
   def go(direction); end
 end
