ce8e05a58a5e579fb348572d8c14dffce32709de
nickkaylarson <nicholaskaylarson@gmail.com>
2021-07-28 04:09:29 -0500

    add send_train, trains_types methods

--- Lesson_3/models/station.rb
+++ Lesson_3/models/station.rb
@@ -14,9 +14,25 @@ class Station
     @trains.store(train.number, train)
   end
 
-  def types_of_trains; end
-
   def delete_train(train)
-    @trains.delete(train.name)
+    @trains.delete(train.number)
+  end
+
+  def trains_types
+    cargos = 0
+    passengers = 0
+    @trains.each_value do |train|
+      if train.type == :cargo
+        cargos += 1
+      else
+        passengers += 1
+      end
+    end
+    { cargos: cargos, passengers: passengers }
+  end
+
+  def send_train(train, station)
+    delete_train(train)
+    station.trains = train
   end
 end
