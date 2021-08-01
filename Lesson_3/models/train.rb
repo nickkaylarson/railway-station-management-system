74ef307fac08446a0d86b3f7a8dcbbae3c0b9cb6
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-01 13:11:50 -0500

    correct move methods

--- Lesson_3/models/train.rb
+++ Lesson_3/models/train.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 require_relative('./route')
+require_relative('./station')
 
 class Train
-  attr_reader :number, :type, :wagons_amount
+  attr_reader :number, :type, :wagons_amount, :route
 
-  attr_accessor :speed, :route
+  attr_accessor :speed
 
   def initialize(number, type, wagons_amount)
     return nil unless (type == :cargo) || (type == :passenger)
@@ -28,33 +29,30 @@ class Train
     end
   end
 
+  def route=(route)
+    @route = route
+    @route.first.trains << self
+  end
+
   def current_station
     @route.each do |station|
       return station if self == station.trains.select { |train| train.number == @number }.pop
-        
-      
     end
     nil
   end
 
   def next_station
     @route.each_with_index do |station, index|
-      if station == current_station 
-        unless (@route[index + 1]).nil?
-        return @route[index + 1] 
-        end
-      end
+      next unless station == current_station
+      return @route[index + 1] if index + 1 < @route.size
     end
     false
   end
 
   def previous_station
     @route.each_with_index do |station, index|
-      if station == current_station 
-        unless (@route[index - 1]).nil?
-        return @route[index - 1] 
-        end
-      end
+      next unless station == current_station
+      return @route[index - 1] if index - 1 >= 0
     end
     false
   end
@@ -64,7 +62,6 @@ class Train
     when :forward
       next_station ? move_train_forward : 'The train is already at the final station!'
     when :backwards
-      binding.irb
       previous_station ? move_train_backwards : 'The train is already at the first station!'
     else
       p 'Train can only move :forward and :backwards!'
@@ -74,13 +71,13 @@ class Train
   private
 
   def move_train_forward
-    next_station.trains << self
-    current_station.trains.delete(self)
+    next_station.trains = self
+    current_station.delete_train(self)
   end
 
   def move_train_backwards
-    binding.irb
-    previous_station.trains << self
-    current_station.trains.delete(self)
+    tmp_station = current_station
+    previous_station.trains = self
+    tmp_station.delete_train(self)
   end
 end
