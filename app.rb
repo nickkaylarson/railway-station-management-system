b7eb414e086e825959c21e6748a1408c6b20f01c
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-04 07:17:41 -0500

    add starting functionality for console app

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'tty-prompt'
+
 require_relative('./models/route')
 require_relative('./models/train')
 require_relative('./models/station')
@@ -68,36 +70,68 @@ require_relative('./models/station')
 # # p train.next_station
 # # binding.irb
 
-
-require 'tty-prompt'
-
 @stations = {}
 
-def create_station
-    name = @prompt.ask('Enter station name:')
-    @stations.store(name, Station.new(name))
-    p @stations
+def show_route
+  if @route.nil?
+    p 'Create route first'
+  else
+    p @route
+  end
 end
 
+def create_stations_menu
+  station_names = []
+  @stations.each_key { |station_name| station_names << station_name }
+  station_names
+end
+
+def create_station
+  name = @prompt.ask('Enter station name:')
+  @stations.store(name, Station.new(name))
+end
+
+def create_route
+  if @stations.empty? || @stations.size == 1
+    p 'Create at least TWO stations first'
+  else
+    start_end_stations = []
+    loop do
+      start_end_stations = @prompt.multi_select('Select the start and end station: ',
+                                                create_stations_menu)
+      if start_end_stations.size == 2
+        break
+      else
+        p 'Choose TWO stations'
+      end
+    end
+    @route = Route.new(@stations[start_end_stations.first], @stations[start_end_stations.last])
+  end
+end
 
 @prompt = TTY::Prompt.new
 
 loop do
-    cmd = @prompt.select("Select an action: ", per_page: 8) do |menu|
-        menu.choice "Create station", 1
-        menu.choice "Create train", 2, disabled: ''
-        menu.choice "Create route", 3, disabled: ''
-        menu.choice "Assign a train route", 4, disabled: ''
-        menu.choice "Manipulations with wagons", 5, disabled: ''
-        menu.choice "Move train", 6, disabled: ''
-        menu.choice "Show route and trains", 7, disabled: ''
-        menu.choice "Exit", 8        
-      end
-  
+  cmd = @prompt.select('Select an action: ', per_page: 9) do |menu|
+    menu.choice 'Create station', 1
+    menu.choice 'Create train', 2, disabled: ''
+    menu.choice 'Create route', 3
+    menu.choice 'Add stations to route', 4
+    menu.choice 'Assign a train route', 5, disabled: ''
+    menu.choice 'Manipulations with wagons', 6, disabled: ''
+    menu.choice 'Move train', 7, disabled: ''
+    menu.choice 'Show route and trains', 8
+    menu.choice 'Exit', 9
+  end
+
   case cmd
-    when 1
-      create_station
-    when 8
-      break if @prompt.yes?('Do you really want to exit?')
+  when 1
+    create_station
+  when 3
+    create_route
+  when 8
+    show_route
+  when 9
+    break if @prompt.yes?('Do you really want to exit?')
   end
 end
