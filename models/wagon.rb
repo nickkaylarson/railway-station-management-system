a447857df75959b7495920365d769bc778ba5680
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-07 07:48:25 -0500

    add manufacturer module to train model

--- models/wagon.rb
+++ models/wagon.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require_relative('../modules/manufacturer')
+
 class Wagon
   attr_reader :number
+
+  include Manufacturer
 
   def initialize(number)
     @number = number
