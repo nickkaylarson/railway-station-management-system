612a8dc619575276498e2bfce510520705a5ae5d
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-13 06:13:33 -0500

    add number validation + valid?

--- models/wagon.rb
+++ models/wagon.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
 require_relative('../modules/manufacturer')
+require_relative('../modules/object_validator')
+
+WAGON_NUMBER_TEMPLATE = /^\d+$/.freeze
 
 class Wagon
   attr_reader :number
 
   include Manufacturer
+  extend ObjectValidator
 
   def initialize(number)
     @number = number
+    validate!
+  end
+
+  private
+
+  def validate!
+    raise 'The number must consist of digits' unless WAGON_NUMBER_TEMPLATE.match?(@number)
   end
 end
