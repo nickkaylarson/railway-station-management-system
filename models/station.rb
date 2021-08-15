d344f318dc9120dd4e4098e5cad7d9a8cdd175f8
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-15 11:34:51 -0500

    add validation for station name

--- models/station.rb
+++ models/station.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 require_relative('../modules/instance_counter')
+require_relative('../modules/object_validator')
 
 class Station
   attr_reader :name, :trains
 
   include InstanceCounter
+  extend ObjectValidator
 
   def initialize(name)
-    @name = name
+    @name = name.to_s
     @trains = []
     self.class.all << self
+    validate!
     register_instance
   end
 
@@ -28,5 +31,11 @@ class Station
 
   def trains_by_type(type)
     @trains.select { |train| train.type == type }
+  end
+
+  private
+
+  def validate!
+    raise 'The station name must consist of at least one character' unless @name.size >= 1
   end
 end
