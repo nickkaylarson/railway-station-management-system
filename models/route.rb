9c4a1c9d172e0887381aca73685ace4579ea03b0
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-16 07:05:53 -0500

    add validation to route model

--- models/route.rb
+++ models/route.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 require_relative('../modules/instance_counter')
+require_relative('../modules/object_validator')
 
 class Route
   attr_reader :starting_station, :end_station, :intermediate_stations
 
   include InstanceCounter
+  extend ObjectValidator
 
   def initialize(starting_station, end_station)
     @starting_station = starting_station
     @end_station = end_station
     @intermediate_stations = []
+    validate!
     register_instance
   end
 
@@ -30,5 +33,11 @@ class Route
     end
     stations << @end_station
     stations
+  end
+
+  private
+
+  def validate!
+    raise 'Stations should not be nil' unless @starting_station != nil || @end_station != nil
   end
 end
