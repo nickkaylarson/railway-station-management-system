ea66fd3290306b5c663259b6e95d92d453d8f506
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-19 12:14:17 -0500

    add occupied_volume attribute to cargo wagon

--- models/cargo_wagon.rb
+++ models/cargo_wagon.rb
@@ -3,9 +3,12 @@
 require_relative('./wagon')
 
 class CargoWagon < Wagon
+  attr_reader :volume
+
   def initialize(number, volume)
     super(number)
     @type = :cargo
     @volume = volume
+    @occupied_volume = 0
   end
 end
