bf1b3f6e2427046d3a59a2c212a5dc26a16eaeca
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-19 12:14:54 -0500

    add occupied_seats attribute to passenger wagon

--- models/passenger_wagon.rb
+++ models/passenger_wagon.rb
@@ -3,9 +3,12 @@
 require_relative('./wagon')
 
 class PassengerWagon < Wagon
+  attr_reader :seats_amount
+
   def initialize(number, seats_amount)
     super(number)
     @type = :passenger
     @seats_amount = seats_amount
+    @occupied_seats = 0
   end
 end
