346c6ad56b31fcedcc86c37b4418a5fd8943b649
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-20 12:37:52 -0500

    add free_volume_amount and occupy_volume methods

--- models/cargo_wagon.rb
+++ models/cargo_wagon.rb
@@ -3,12 +3,26 @@
 require_relative('./wagon')
 
 class CargoWagon < Wagon
-  attr_reader :volume
+  attr_reader :volume, :occupied_volume
 
   def initialize(number, volume)
     super(number)
     @type = :cargo
     @volume = volume
     @occupied_volume = 0
+  end
+
+  def free_volume_amount
+    @volume - @occupied_volume
+  end
+
+  private
+
+  def occupy_volume(volume_to_occupy)
+    if (@volume - volume_to_occupy).negative?
+      p 'There are no more available volume!'
+    else
+      @occupied_volume += volume_to_occupy
+    end
   end
 end
