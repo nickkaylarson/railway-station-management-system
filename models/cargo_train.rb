042891d44c5a3a00236250aa817e2059556daf4e
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-29 05:53:08 -0500

    add validation module

--- models/cargo_train.rb
+++ models/cargo_train.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 require_relative('./train')
+require_relative('../modules/validation')
 
 class CargoTrain < Train
+  include Validation
+
+  validate :number, presence: true, format: /^\w{3}-{0,1}\w{2}/
+
   def initialize(number)
     super(number)
     @type = :cargo
+    validate!
   end
 end
