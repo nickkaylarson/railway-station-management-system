60632597f929aea5819975e7ed41faa5e9475c4a
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-29 04:20:38 -0500

    add check_type, valid?, refactor

--- modules/validation.rb
+++ modules/validation.rb
@@ -1,57 +1,64 @@
 # frozen_string_literal: true
 
 module Validation
-    def self.included(base)
-      base.extend ClassMethods
-      base.include InstanceMethods
+  def self.included(base)
+    base.extend ClassMethods
+    base.include InstanceMethods
+  end
+
+  module ClassMethods
+    def validate(attribute_name, options = {})
+      @attribute_name = attribute_name
+      @options = {
+        presence: options[:presence] || nil,
+        format: options[:format] || nil,
+        type: options[:type] || nil
+      }
     end
-  
-    module ClassMethods
-      def validate(object, attribute_name, options = {})
-        @object = object
-        @attribute_name = attribute_name        
-        @options = {
-          presence: options[:presence] || nil, 
-          format: options[:format] || nil, 
-          type: options[:type] || nil
-        }
-        
-        @options.compact!.each_key do |key| 
-          case key
-          when :presence
-            check_presence
-          when :format
-            check_format
-          when :type
-            check_type
-          end
-        end
-      end
-      
-      private
-      
-      def check_presence
-        if @object.instance_variable_get("@#{@attribute_name.to_s}".to_sym).nil?
-          raise "#{@object.class}.#{@attribute_name.to_s} shouldn't be nil"
-        elsif @object.instance_variable_get("@#{@attribute_name.to_s}".to_sym) == ''
-          raise "#{@object.class}.#{@attribute_name.to_s} shouldn't be empty"
-        end
-      end
 
-      def check_format
-        raise "Incorrect format! Should match: #{@options[:format]}" unless @options[:format].match?(@object.instance_variable_get("@#{@attribute_name.to_s}".to_sym))
-      end
+    private
 
-      def check_type
+    def check_presence(object)
+      if object.instance_variable_get("@#{@attribute_name.to_s}".to_sym).nil?
+        raise "#{object.class}.#{@attribute_name} shouldn't be nil"
+      elsif object.instance_variable_get("@#{@attribute_name.to_s}".to_sym).empty?
+        raise "#{object.class}.#{@attribute_name} shouldn't be empty"
       end
     end
-  
-    module InstanceMethods
-      def validate!(attribute_name, options = {})
-        self.class.validate(self, attribute_name, options)
+
+    def check_format(object)
+      unless @options[:format].match?(object.instance_variable_get("@#{@attribute_name}".to_sym))
+        raise "Incorrect format! Should match: #{@options[:format]}"
       end
-      def valid?
+    end
+
+    def check_type(object)
+      unless object.instance_variable_get("@#{@attribute_name}".to_sym).instance_of?(@options[:type])
+        raise "Wrong type! #{object.class}.#{@attribute_name} should be #{@options[:type]}"
       end
     end
   end
-  
+
+  module InstanceMethods
+    def validate!
+      self.class.instance_variable_get(:@options).compact.each_key do |key|
+        case key
+        when :presence
+          self.class.send(:check_presence, self)
+        when :format
+          self.class.send(:check_format, self)
+        when :type
+          self.class.send(:check_type, self)
+        end
+      end
+    end
+
+    def valid?
+      validate!
+      true
+    rescue StandardError => e
+      p e.message
+      false
+    end
+  end
+end
