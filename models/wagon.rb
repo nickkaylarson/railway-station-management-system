0598f3d4d0dbd3ade0ed5507f6dc61679118830c
nickkaylarson <nicholaskaylarson@gmail.com>
2021-08-29 05:16:56 -0500

    remove validation from super class

--- models/wagon.rb
+++ models/wagon.rb
@@ -1,25 +1,13 @@
 # frozen_string_literal: true
 
 require_relative('../modules/manufacturer')
-require_relative('../modules/object_validator')
 
 class Wagon
-  WAGON_NUMBER_TEMPLATE = /^\d+$/.freeze
-  VALIDATION_MESSAGE = 'The number must consist of digits'
-
   attr_reader :number
 
   include Manufacturer
-  extend ObjectValidator
 
   def initialize(number)
     @number = number
-    validate!
-  end
-
-  private
-
-  def validate!
-    raise VALIDATION_MESSAGE unless WAGON_NUMBER_TEMPLATE.match?(@number)
   end
 end
