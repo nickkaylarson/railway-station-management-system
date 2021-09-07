57d435830ae7d4828dd4e36478b3570ceef9a3fb
nickkaylarson <nicholaskaylarson@gmail.com>
2021-09-07 11:03:33 -0500

    move interface into separate class

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-require_relative('interface')
+require_relative('managment_system')
 
-Interface.new.start if __FILE__ == $PROGRAM_NAME
+ManagmentSystem.new.start if __FILE__ == $PROGRAM_NAME
---
+++ managment_system.rb
@@ -0,0 +1,326 @@
+# frozen_string_literal: true
+
+require_relative('interface')
+
+require_relative('./models/route')
+require_relative('./models/station')
+require_relative('./models/cargo_train')
+require_relative('./models/cargo_wagon')
+require_relative('./models/passenger_train')
+require_relative('./models/passenger_wagon')
+
+class ManagmentSystem
+  def initialize
+    @stations = []
+    @trains = []
+  end
+
+  def start
+    @interface = Interface.new
+    menu_loop
+  end
+
+  private
+
+  def find_station(station_name)
+    @stations.find { |station| station if station.name == station_name }
+  end
+
+  def check_station(station)
+    @route.starting_station == find_station(station) ||
+      @route.end_station == find_station(station) ||
+      @route.intermediate_stations.include?(find_station(station))
+  end
+
+  def create_stations_menu
+    station_names = []
+    @stations.each { |station| station_names << station.name }
+    station_names
+  end
+
+  def add_intermediate_stations
+    if @route.nil?
+      p 'Create route first'
+    else
+      intermediate_stations = create_stations_menu
+      intermediate_stations.each do |intermediate_station|
+        if check_station(intermediate_station)
+          p 'Choose another stations!'
+          p 'These stations are already on the route!'
+        else
+          @route.intermediate_stations = find_station(intermediate_station)
+          break
+        end
+      end
+    end
+  end
+
+  def show_route
+    if @route.nil?
+      p 'Create route first'
+    else
+      p @route.stations
+    end
+  end
+
+  def create_trains_menu
+    train_numbers = []
+    @trains.each { |train| train_numbers << train.number }
+    train_numbers
+  end
+
+  def create_station
+    @stations << Station.new(@prompt.ask('Enter station name:'))
+  rescue StandardError => e
+    p e.message
+  end
+
+  def new_route(first_station, last_station)
+    @route = Route.new(first_station, last_station)
+  rescue StandardError => e
+    p e.message
+  end
+
+  def create_route
+    if @stations.empty? || @stations.size == 1
+      p 'Create at least TWO stations first'
+    else
+      start_end_stations = []
+      loop do
+        start_end_stations = @prompt.multi_select('Select the start and end station: ',
+                                                  create_stations_menu)
+        if start_end_stations.size == 2
+          break
+        else
+          p 'Choose TWO stations'
+        end
+      end
+      new_route(find_station(start_end_stations.first), find_station(start_end_stations.last))
+    end
+  end
+
+  def choose_train_type
+    @prompt.select('Choose train type: ', %w[cargo passenger])
+  end
+
+  def create_cargo_train
+    @trains << CargoTrain.new(@prompt.ask('Please, enter train number: '))
+  rescue StandardError => e
+    p e.message
+    retry
+  end
+
+  def create_passenger_train
+    @trains << PassengerTrain.new(@prompt.ask('Please, enter train number: '))
+  rescue StandardError => e
+    p e.message
+    retry
+  end
+
+  def create_train
+    case choose_train_type
+    when 'cargo'
+      create_cargo_train
+    when 'passenger'
+      create_passenger_train
+    end
+  end
+
+  def select_train_number
+    @prompt.select('Select train: ', create_trains_menu)
+  end
+
+  def find_train(train_number)
+    @trains.find { |train| train if train.number == train_number }
+  end
+
+  def assign_route
+    if @route.nil?
+      p 'Create route first'
+    elsif @trains.empty?
+      p 'Create at least one train first'
+    else
+      find_train(select_train_number).route = @route
+    end
+  end
+
+  def move(train)
+    directions = %w[forward backwards]
+    case @prompt.select('Choose direction: ', directions)
+    when 'forward'
+      train.move(:forward)
+    when 'backwards'
+      train.move(:backwards)
+    end
+  end
+
+  def move_train
+    if @trains.empty?
+      p 'Create at least one train first'
+    else
+      train_number = select_train_number
+      train = find_train(train_number)
+      case @prompt.select('Select option: ', ['change speed', 'stop', 'move'])
+      when 'change speed'
+        train.speed = @prompt.ask('Enter speed > 0:').to_i
+      when 'stop'
+        train.stop
+      when 'move'
+        move(train)
+      end
+    end
+  end
+
+  def find_wagon(train, wagon_number)
+    train.wagons.find { |wagon| wagon if wagon.number == wagon_number }
+  end
+
+  def add_cargo_wagon(train, wagon_number)
+    volume = @prompt.ask('Enter amount of wagon volume: ').to_f
+    begin
+      train.add_wagon(CargoWagon.new(wagon_number, volume))
+    rescue StandardError => e
+      p e.message
+    end
+  end
+
+  def add_passenger_wagon(train, wagon_number)
+    seats_amount = @prompt.ask('Enter amount of wagon seats: ')
+    begin
+      train.add_wagon(PassengerWagon.new(wagon_number, seats_amount))
+    rescue StandardError => e
+      p e.message
+    end
+  end
+
+  def add_remove_wagons
+    if @trains.empty?
+      p 'Create at least one train first'
+    else
+      train_number = select_train_number
+      train = find_train(train_number)
+      if train.speed.positive?
+        p 'Stop the train first!'
+      else
+        wagon_number = @prompt.ask('Enter wagon number: ')
+
+        case @prompt.select('Select option: ', ['add wagon', 'delete wagon'])
+        when 'add wagon'
+          if train.type == :cargo
+            add_cargo_wagon(train, wagon_number)
+          else
+            add_passenger_wagon(train, wagon_number)
+          end
+        when 'delete wagon'
+          train.delete_wagon(find_wagon(train, wagon_number))
+        end
+      end
+    end
+  end
+
+  def create_wagons_menu(train_number)
+    wagons_numbers = []
+    find_train(train_number).wagons.each { |wagon| wagons_numbers << wagon.number }
+    wagons_numbers
+  end
+
+  def enter_manufacturer
+    @prompt.ask('Enter manufacturer: ')
+  end
+
+  def add_manufacturer
+    case @prompt.select('Choose wagons or trains: ', %w[wagons trains])
+    when 'wagons'
+      train_number = select_train_number
+      train = find_train(train_number)
+      if  train.wagons.empty?
+        p 'Add wagons to train first!'
+      else
+        wagon_number = @prompt.select('Select wagon: ', create_wagons_menu(train_number))
+        find_wagon(train, wagon_number).manufacturer = enter_manufacturer
+      end
+    when 'trains'
+      train_number = select_train_number
+      train = find_train(train_number)
+      train.manufacturer = enter_manufacturer
+    end
+    p train
+  end
+
+  def occupy
+    if @trains.empty?
+      p 'Create at least one train first'
+    else
+      train_number = select_train_number
+      train = find_train(train_number)
+      if  train.wagons.empty?
+        p 'Add wagons to train first!'
+      else
+        wagon_number = @prompt.select('Select wagon: ', create_wagons_menu(train_number))
+        case train.type
+        when :cargo
+          find_wagon(train, wagon_number).occupy_volume(@prompt.ask('Enter volume: ').to_f)
+        when :passenger
+          find_wagon(train, wagon_number).occupy_seat
+        end
+      end
+    end
+  end
+
+  def print_trains_on_station
+    station_name = @prompt.select('Select station: ', create_stations_menu)
+    station = find_station(station_name)
+    station.return_trains_for_print do |train|
+      p "Train number: #{train.number}, train type: #{train.type}, wagons amount: #{train.wagons.size}"
+    end
+  end
+
+  def print_train_wagons
+    train_number = select_train_number
+    train = find_train(train_number)
+
+    if train.type == :cargo
+      train.return_wagons_for_print do |wagon|
+        p "Wagon number: #{wagon.number}, wagon type: #{train.type}, free volume amount: #{wagon.free_volume_amount}, occupied volume amount: #{wagon.occupied_volume}"
+      end
+    else
+      train.return_wagons_for_print do |wagon|
+        p "Wagon number: #{wagon.number}, wagon type: #{train.type}, free seats amount: #{wagon.free_seats_amount}, occupied volume amount: #{wagon.occupied_seats}"
+      end
+    end
+  end
+
+  def menu_loop
+    loop do
+      case @interface.make_choice
+      when 1
+        create_station
+      when 2
+        create_train
+        p @trains
+      when 3
+        create_route
+      when 4
+        add_intermediate_stations
+      when 5
+        assign_route
+      when 6
+        add_remove_wagons
+      when 7
+        move_train
+      when 8
+        occupy
+      when 9
+        show_route
+      when 10
+        add_manufacturer
+      when 11
+        print_trains_on_station
+      when 12
+        print_train_wagons
+      when 13
+        break if @interface.exit?
+      end
+    end
+  end
+end
